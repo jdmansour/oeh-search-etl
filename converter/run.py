7a71671c4a1d87c8720862c68d17b06d1e105c17
Jason Mansour <jason.mansour@gwdg.de>
2023-11-17 12:24:27 +0100

    Combine Scrapy and Uvicorn into one process

--- converter/run.py
+++ converter/run.py
@@ -1,22 +1,31 @@
 """
-Debugging entry point for VSCode.
+Example showing how to run Scrapy and Uvicorn in the same process.
 
-Add the following to the `configurations` array in `.vscode/launch.json`:
+This is probably not a good idea :-) but here's how if you need it.
 
-    {
-        "name": "Run scrapy",
-        "type": "python",
-        "request": "launch",
-        "program": "${workspaceFolder}/converter/run.py",
-        "console": "integratedTerminal"
-    }
+Usage:
+
+    $ . .venv/bin/activate
+    $ python -m converter.run
+
+Then the Swagger UI will be at http://127.0.0.1:8000/docs .
+
+This works by making both Uvicorn and Scrapy use asyncio as their backend.
+Scrapy is based on Twisted. We use the twisted asyncioreactor. When that
+is running, we start the uvicorn server. Then when we get the POST request,
+we can just launch Scapy via CrawlerRunner (not CrawlerProcess).
+
+Note that you cannot call this script via something like
+
+    uvicorn --factory converter.run:create_app
+
+since we need to start the reactor before the uvicorn server.
 """
 
 
 import os
 from scrapy.cmdline import execute
 
-import asyncio
 import uvicorn
 import fastapi as fapi
 import fastapi.middleware.cors as fapicors
@@ -26,7 +35,8 @@ import scrapy.utils.project as sproj
 from converter.spiders.generic_spider import GenericSpider
 from scrapy import signals
 from scrapy.signalmanager import dispatcher
-import json
+from scrapy.utils.defer import deferred_to_future, deferred_from_coro
+from scrapy.utils.serialize import ScrapyJSONEncoder
 
 
 
@@ -74,35 +84,46 @@ def create_app() -> fapi.FastAPI:
     @app.post("/metadata")
     async def metadata(data: Data) -> Result:
         print("TARGET_URL", data.url)
+
         project_settings = sproj.get_project_settings()
         results = []
         def crawler_results(signal, sender, item, response, spider):
-            # results.append(item)
-            # dict_result = dict(results[0])
-            # json_results = json.dumps(str(dict_result), indent=4)
-            return Result(
-                resultjson="json_results",
-            )
+            results.append(item)
 
         dispatcher.connect(crawler_results, signal=signals.item_scraped)
 
-        c = cw.CrawlerProcess(project_settings)
-        GenericSpider.start_urls = ['https://de.wikipedia.org/wiki/G%C3%B6ttingen']
-        c.crawl(GenericSpider)
-        # list_ports = os.system("lsof -i :5500")
-        # if list_ports != 0:
-        c.start()
+        url = 'https://de.wikipedia.org/wiki/G%C3%B6ttingen'
+        c = cw.CrawlerRunner(project_settings)
+        # Convert the 'deferred' to something we can 'await':
+        await deferred_to_future(c.crawl(GenericSpider, url=url))
+
+        lom = results[0]['lom']
+        encoder = ScrapyJSONEncoder()
+        resultjson = encoder.encode(lom)
+
+        return Result(
+            # why do we put this as a json encoded string into the response, and not directly as json?
+            resultjson=resultjson,
+        )
 
     return app
 
-async def start_ws_service():
-    config = uvicorn.Config("run:create_app", port=5500, log_level="info")
+
+def start_ws_service(*args):
+    config = uvicorn.Config("converter.run:create_app", port=8000, log_level="info", loop="asyncio", factory=True)
     server = uvicorn.Server(config)
-    await server.serve()
+    print("calling serve")
+    
+    from twisted.internet import reactor
+    d = deferred_from_coro(server.serve())
+    # shutdown Twisted reactor (and the whole process) when the web server shuts down
+    d.addBoth(lambda *args: reactor.stop())
 
 
 if __name__ == "__main__":
-    # run()
-    list_ports = os.system("lsof -i :5500")
-    if list_ports != 0:
-        asyncio.run(start_ws_service())
+    from twisted.internet import asyncioreactor
+    asyncioreactor.install()
+
+    from twisted.internet import reactor
+    reactor.callWhenRunning(start_ws_service)
+    reactor.run()
